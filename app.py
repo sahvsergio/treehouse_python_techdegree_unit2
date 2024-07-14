108eb44e0c0b35a27e6852af3c2cfdf768ddaff8
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-07-14 01:13:43 -0500

    added app.py

---
+++ app.py
@@ -0,0 +1,91 @@
+
+from constants import PLAYERS
+from constants import TEAMS
+import copy
+
+def main():
+    pass
+
+def display_menu():
+    print('BASKETBALL TEAM STATS TOOL'.center(127))
+    print('-'*3,'MENU'.center(127),'-'.*3)
+    
+
+
+
+
+
+
+
+       
+
+   
+    #read the existing player data
+    #clean the player data using copy.deepcopy()
+    #save the cleaned data to a new collection
+
+    ##data to be cleaned :
+    #height= should be an integer
+    #experience= this should be boolean
+    #Ensure you do not directly modify the data in PLAYERS or 
+    # TEAMS constants. This data you should iterate and read 
+    # from to build your own collection and would be ideal to clean the data 
+    # as you loop over it building your new collection.
+
+def balance_teams(**kwargs):
+    pass
+    #unction to balance the players across the three teams: 
+    # Panthers, Bandits, and Warriors. 
+    # Make sure the teams have the same number of total players
+    # on them when your team balancing function has finished.
+
+
+#HINT: To find out how many players should be on each team, divide the length of players by the number of teams:
+
+#display():
+#"hen the menu or stats display to the console, 
+#it should display in a nice readable format. Use extra spaces or line breaks('\n') 
+# to break up lines if needed. For example, '\nThis will start on a newline.'
+
+
+#the script should not crash due to  uncaught exception
+
+
+
+# logic should be wrapped inside a Dunder Main statement for your script.
+
+
+
+# Do not modify or mutate the imported data from constants.py in any way.
+
+#HINT: You will want to iterate over this data and create a new data structure to hold your cleaned data.
+
+# Assign players to each team so the teams are evenly balanced by total players.
+
+
+
+
+
+#The order in which you assign the players do not matter but should be balanced when team assignment is finished.
+
+#The same player cannot be assigned to multiple teams.
+
+#As a user, I should be able to respond to a menu with each of the following options:
+
+#    Display a given teams stats
+#    Quit the program
+
+#Actual names of the displayed options are up to you but must make sense.
+if __name__=='__main__':
+    # Most of the code should be inside functions or classes
+    # Use dunder main to control your code execution
+    # Use a function called main() to contain code you want to run and call other functions from main()
+
+    
+    #read the existing player data
+   #player_list=copy.deepcopy(PLAYERS)
+  
+   
+   #print(type(clean_data(player_list)))
+   display_menu()
+   
