ead715b658cb66ca52c162b2a6d39ee8aacfc222
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-07-16 20:43:12 -0500

    organized entry point

--- app.py
+++ app.py
@@ -12,19 +12,25 @@ def main():
      #read data for both teams and players
      player_list = copy.deepcopy(PLAYERS)
      teams=copy.deepcopy(TEAMS)
-     #
+     #clean
      cleaned_players= clean_data(player_list)
-     print(cleaned_players)
+    
      
-     #team_length=len(cleaned_players)//len(teams)
-     #balanced_teams=balance_teams(cleaned_players, teams, team_length)
+   
+     team_length = len(cleaned_players)//len(teams)
+     balanced_teams = balance_teams(teams,cleaned_players, team_length)
+     #team=greet(teams)
+    
+   
+     
+     
     
     
    
 
 if __name__=='__main__':
     main()
-   
+    
 
 
 
