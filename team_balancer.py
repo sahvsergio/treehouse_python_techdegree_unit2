3c74e589c1aef91d94c686d6f31e40ff7bd128c0
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-07-18 18:28:12 -0500

    reverted to keeping even teams

--- team_balancer.py
+++ team_balancer.py
@@ -3,61 +3,36 @@ import copy
 import random
 
 
-
-def balance_teams(teams, cleaned_players,team_length):
+def balance_teams(teams, cleaned_players, team_length):
     """_summary_
 
     Args:
         teams (_type_): _description_
         
     """
-    #creating an empty dictionary to hold balanced  player teams
-    balanced_teams={}
-    #loop through the teams
+    # creating an empty dictionary to hold balanced  player teams
+    balanced_teams = {}
+    # loop through the teams
     for team in teams:
-        
-        # making the teams the keys of the dictionary, 
+        # making the teams the keys of the dictionary,
         # initializing them with a value of an empty list
-        balanced_teams[team]=[]
-        player_index=0
-        while player_index<len(cleaned_players):
-            #looping through the list of players:
-            for team in teams: 
-                #appending the player to the team if 
-                if len(balanced_teams[team])<team_length:
-                    balanced_teams[team].append(cleaned_players[player_index])
-                    player_index=+1
-                    if player_index>=len(cleaned_players):
-                        break
-                    
+        balanced_teams[team] = []
+        # looping through the list of players:
+        for player in cleaned_players:
+            # appending the player to the team
+            if len(balanced_teams[team]) == 0:
+                balanced_teams[team].append(player)
                 
-           
-            
-
-        
-            
-                
-        
-                    
-        
-                    
-            
-        
-                    
-                    
                
-    
-         
-                
-                
-                    
-                    
-                    
-                
-                
-                    
-                
+
+            elif len(balanced_teams[team]) < team_length:
+                status = balanced_teams[team][-1]['Experience']
+                if status != player['Experience']:
+                    balanced_teams[team].append(player)
+               
             
+        
+
     print(f"the panthers, roar{balanced_teams['Panthers']}")
     print()
     print()
@@ -65,10 +40,7 @@ def balance_teams(teams, cleaned_players,team_length):
     print()
     print()
     print(f" The warriors: {balanced_teams['Warriors']}")
-    
-    
+
     print(len(balanced_teams['Bandits']))
     print(len(balanced_teams['Warriors']))
     print(len(balanced_teams['Panthers']))
-    
-        
