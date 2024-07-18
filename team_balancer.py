350293d230f9a261c283c8dbb76d7664191984e4
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-07-17 20:56:18 -0500

    entered equal elements in each team

--- team_balancer.py
+++ team_balancer.py
@@ -10,24 +10,23 @@ def balance_teams(teams, cleaned_players,team_length):
         teams (_type_): _description_
         
     """
+    #creating an empty dictionary to hold balanced  player teams
     balanced_teams={}
+    #loop through the teams
     for team in teams:
-      balanced_teams[team]=' '
-    #print(balanced_teams)
-    for player in cleaned_players:
-        balanced_teams[team]=player
-    print(balanced_teams)
-
-    
-        
-        
-        
-        
-        
-   
-    
- 
+        # making the teams the keys of the dictionary, 
+        # initializing them with a value of an empty list
+        balanced_teams[team]=[]
+        #looping through the list of players:
+        for player in cleaned_players:
+            #appending 
+            balanced_teams[team].append(player)
+            if len(balanced_teams[team])<team_length :
             
-    
-            
+                continue
+            else:
+                break
+    print(len(balanced_teams['Panthers']))
+    print(len(balanced_teams['Bandits']))
+    print(len(balanced_teams['Warriors']))
         
