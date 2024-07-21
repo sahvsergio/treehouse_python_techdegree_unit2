bae6f26c87eb213b5f2fb55035dcc2f57e602573
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-07-20 22:07:50 -0500

    there are different players in each team

--- team_balancer.py
+++ team_balancer.py
@@ -4,31 +4,30 @@ import random
 
 
 def balance_teams(teams, cleaned_players, team_length):
-    """_summary_
 
-    Args:
-        teams (_type_): _description_
-
-    """
-    # creating an empty dictionary to hold balanced  player teams
     balanced_teams = {}
-    # loop through the teams
     for team in teams:
-        # making the teams the keys of the dictionary,
-        # initializing them with a value of an empty list
         balanced_teams[team] = []
-        # looping through the list of players:
-        for player in cleaned_players:
-            # appending the player to the team
-            if len(balanced_teams[team]) == 0:
-                balanced_teams[team].append(player)
-                cleaned_players.pop(0)
+        if len(cleaned_players) > 0:
+            index_player = 0
+            for player in cleaned_players:
+                while len(balanced_teams[team]) < team_length:
+                    print(len(cleaned_players))
+                    if player['First Name'] not in balanced_teams[team]:
 
-            elif len(balanced_teams[team]) < team_length:
-                status = balanced_teams[team][-1]['Experience']
-                if status != player['Experience']:
-                    balanced_teams[team].append(player)
-    cleaned_players.pop(0)
+                       popped = cleaned_players.pop(0)
+                       balanced_teams[team].append(popped)
+
+                       print(popped)
+                       print(len(cleaned_players))
+
+                       continue
+                else:
+                    break
+        elif len(cleaned_players) == 0:
+            break
+        else:
+            continue
 
     print(f"the panthers, roar{balanced_teams['Panthers']}")
     print()
@@ -43,6 +42,5 @@ def balance_teams(teams, cleaned_players, team_length):
 
     with open('team_balancer.txt', 'w') as my_file:
         my_file.write(f"the panthers, roar{balanced_teams['Panthers']}")
+        my_file.write(f" the bandits{balanced_teams['Bandits']}")
         my_file.write(f" The warriors: {balanced_teams['Warriors']}")
-        my_file.write(f" The warriors: {balanced_teams['Warriors']}")
-
